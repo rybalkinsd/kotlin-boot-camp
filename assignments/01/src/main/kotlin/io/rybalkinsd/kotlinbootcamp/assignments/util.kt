8fc12e846ab7e578fb2ea2b05f75e402e4d55da6
rybalkinsd <yan.brikl@gmail.com>
2018-09-27 18:27:16 +0300

    assignment #1

---
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -0,0 +1,17 @@
+package io.rybalkinsd.kotlinbootcamp.assignments
+
+/**
+ * Returns the greatest of `int` values.
+ *
+ * @param values an argument. !! Assume values.length > 0. !!
+ * @return the largest of values.
+ */
+fun max(values: List<Int>): Int = TODO()
+
+/**
+ * Returns the sum of all `int` values.
+ *
+ * @param values an argument. Assume values.length > 0.
+ * @return the sum of all values.
+ */
+fun sum(values: List<Int>): Long = TODO()
