16cc006c8f869fa87d9a7e6b5d73d088f7fbdd46
rybalkinsd <yan.brikl@gmail.com>
2018-09-28 14:16:01 +0300

    lecture 02 # practice basics - make list

---
+++ lecture02/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/practice/ProducerTest.kt
@@ -0,0 +1,22 @@
+package io.rybalkinsd.kotlinbootcamp.practice
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class ProducerTest {
+
+    @Test
+    fun `make a simple list`() {
+        assertEquals(listOf(1, 2, 3, 4), makeList(1, 4))
+    }
+
+    @Test
+    fun `make list with single element`() {
+        assertEquals(listOf(1), makeList(1, 1))
+    }
+
+    @Test
+    fun `make list with broken bounds`() {
+        assertEquals(emptyList<Int>(), makeList(1, 0))
+    }
+}
