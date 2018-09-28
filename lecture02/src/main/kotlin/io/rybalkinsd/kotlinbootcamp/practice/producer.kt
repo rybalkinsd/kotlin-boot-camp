52f4867f044d553946bf461c1a4ca9731740b724
rybalkinsd <yan.brikl@gmail.com>
2018-09-28 14:16:01 +0300

    lecture 02 # practice basics - make list

---
+++ lecture02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/producer.kt
@@ -0,0 +1,19 @@
+package io.rybalkinsd.kotlinbootcamp.practice
+
+
+/**
+ * Produces a list of sorted integers
+ *
+ * @param from - lower bound of range (including)
+ * @param to - upper bound of range (including)
+ *
+ * @return a sorted list of integers from lower bound to upper bound
+ * example:
+ * makeList(1, 4) = listOf(1, 2, 3)
+ * makeList(1, 1) = listOf(1)
+ * makeList(1, 0) = emptyList()
+ *
+ */
+fun makeList(from: Int, to: Int): List<Int> = TODO()
+
+
