e116796b65583116cafbb2010c3f9c5ed5f494d5
rybalkinsd <yan.brikl@gmail.com>
2018-09-28 14:02:00 +0300

    lecture 02 # practice basics

---
+++ lecture02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/basics.kt
@@ -0,0 +1,27 @@
+package io.rybalkinsd.kotlinbootcamp.practice
+
+
+/**
+ * @param a - first integer
+ * @param b - second integer
+ *
+ * @return math minimum of two integers
+ *
+ * example:
+ * min(1 , 2) = 1
+ * min(-16 , -1000) = -1000
+ */
+fun min(a: Int, b: Int): Int = TODO()
+
+
+/**
+ * Concatenate all strings from
+ * @param values
+ * using a
+ * @param separator
+ *
+ * example:
+ * concat(listOf("a", "b", "c", "d"), "") = "abcd
+ * concat(listOf("1", "2", "3", "4"), " + ") = "1 + 2 + 3 + 4"
+ */
+fun concat(values: List<String>, separator: String): String = TODO()
