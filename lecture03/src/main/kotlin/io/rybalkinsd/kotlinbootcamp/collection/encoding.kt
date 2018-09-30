58a56cc7f2eb96c2cee5afb13627fd2e63f8bf55
rybalkinsd <yan.brikl@gmail.com>
2018-09-30 21:32:28 +0300

    encode practice

---
+++ lecture03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/collection/encoding.kt
@@ -0,0 +1,48 @@
+package io.rybalkinsd.kotlinbootcamp.collection
+
+
+/**
+ * Military alphabet
+ */
+val alphabet = setOf("alpha", "bravo", "charlie")
+
+/**
+ * A mapping for english characters to military alphabet.
+ * [ a -> alpha, b -> bravo, ...]
+ */
+val association: Map<Char, String> = TODO()
+
+/**
+ * Extension function for String which encode it according to `association` mapping
+ *
+ * @return encoded string
+ *
+ * Example:
+ * "abc".encode() == "alphabravocharlie"
+ *
+ */
+fun String.encode(): String = TODO()
+
+
+/**
+ * A reversed mapping for association
+ * [ alpha -> a, bravo -> b, ...]
+ */
+val reversedAssociation: Map<String, Char> = TODO()
+
+
+/**
+ * Extension function for String which decode it according to `reversedAssociation` mapping
+ *
+ * @return encoded string or null if it is impossible to decode
+ *
+ * Example:
+ * "alphabravocharlie".encode() == "abc"
+ * "charliee".decode() == null
+ *
+ */
+fun String.decode(): String? = TODO()
+
+
+
+
