b7e58c4cc61b6b8497b3728645713573492f685c
rybalkinsd <yan.brikl@gmail.com>
2018-10-06 15:18:20 +0300

    lecture04 #initial

---
+++ lecture04/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.gradle.internal.impldep.aQute.bnd.osgi.Analyzer
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "io.rybalkinsd"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.2.71"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+repositories {
+    jcenter()
+}
+
+val ktlint by configurations.creating
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
+    testCompile("junit", "junit", "4.12")
+
+    ktlint("com.github.shyiko", "ktlint", "0.28.0")
+}
+
+tasks {
+    val ktlint by creating(JavaExec::class) {
+        group = "verification"
+        description = "Check Kotlin code style."
+        main = "com.github.shyiko.ktlint.Main"
+        classpath = ktlint
+        args = listOf("src/**/*.kt")
+    }
+
+    "check" {
+        dependsOn(ktlint)
+    }
+}
---
+++ lecture04/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/binaryUtil.kt
@@ -0,0 +1,9 @@
+package io.rybalkinsd.kotlinbootcamp.practice
+
+/**
+ * "8".toBinary() == "1000"
+ *
+ * @throws NumberFormatException if could not be parsed as Number
+ * @throws IllegalArgumentException if target string is null
+ */
+fun String?.toBinary(): String = TODO()
